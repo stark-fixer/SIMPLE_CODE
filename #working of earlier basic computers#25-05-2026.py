399dac705562c0334f290e4da90efe7b00c8253b
stark-fixer <utkarshitjobs@gmail.com>
2026-05-25 15:24:32 +0530

    #working of earlier basic computers#25-05-2026.py
    TIME FLOWS

---
+++ #working of earlier basic computers#25-05-2026.py
@@ -0,0 +1,11 @@
+#inputs from user(basic idea behind ENIAC)
+num_1 = int(input('enter the first number: '))   #CPU will store these values
+num_2 = int(input('enter the second number: '))
+
+#The Process
+output = num_1 + num_2 #CPU operates
+
+#The Output
+print(f"{num_1} + {num_2} = {output}")
+
+#THIS IS HOW, ONE OF THE FIRST COMPUTERS WORKS(LIKE A CALCULATOR)
